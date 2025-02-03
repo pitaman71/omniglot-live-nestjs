69726ebf43fe12c4c7e007943d04304071265463
Alan Pita <pitaman512@gmail.com>
2025-02-03 11:53:26 -0600

    - initial draft

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import configure, { PersistConfig } from './persist.config';
+import { PersistService } from './persist.service';
+import { PersistController } from './persist.controller';
+import { PersistModule } from './persist.module';
+export { configure, PersistService, PersistController, PersistModule };
